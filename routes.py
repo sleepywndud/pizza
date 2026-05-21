33dc770cbbcb41e70555d85e4e3ef41223ee7f13
wndx2 <22222w@duck.com>
2026-05-22 10:33:47 +1200

    refactor: ordering logic
    Previous ordering logic allowed users to get any item for FREE, as ordering passed the price value through the URL. If user modified the URL to make price value zero, they could get pizza for free.
    
    Co-Authored-By: James Park <parkj2@duck.com>

--- routes.py
+++ routes.py
@@ -56,40 +56,60 @@ def update_quantity(item_id):
     return redirect(url_for("main"))
 
 
-@app.route("/add_to_order/<name>/<price>")
-def add_to_order(name, price):
-    conn = sqlite3.connect("order.db")
+@app.route("/add_to_order/<name>")
+def add_to_order(name):
+    conn = sqlite3.connect("database.db")
     cr = conn.cursor()
-    # checks if item is in order.db
-    cr.execute(
-        "SELECT quantity FROM orders WHERE name = ?", (name,)
-    )  # need to pass a tuple so that it works
-    item = cr.fetchone()
 
-    if item:
-        # if item exists, increase the quantity by one
-        new_quantity = item[0] + 1
-        cr.execute(
-            "UPDATE orders SET quantity = ? WHERE name = ?", (new_quantity, name)
-        )
-    else:
-        # if item doesn't exist, then add the row to the db, with quantity 1
-        cr.execute(
-            "INSERT INTO orders (name, price, quantity) VALUES (?, ?, 1)", (name, price)
-        )
+    # check pizza table
+    cr.execute("SELECT price FROM pizza WHERE name = ?", (name,))
+    item_data = cr.fetchone()
 
-    conn.commit()
+    # if not in pizza, check snack table
+    if item_data is None:  # (if not in pizza table)
+        cr.execute("SELECT price FROM snack WHERE name = ?", (name,))
+        item_data = cr.fetchone()  # set item_data as the price of the item
+
+    # NOTE: add other checks as well once other pages are added
+
     conn.close()
+
+    # updates quantity in order table if item is in menu
+    if item_data is not None:
+        price = item_data[0]
+        print(f"\n {price} \n")
+
+        conn = sqlite3.connect("order.db")
+        cr = conn.cursor()
+        # checks if item is already in order.db
+        cr.execute("SELECT quantity FROM orders WHERE name = ?", (name,))
+        item = cr.fetchone()
+
+        if item is not None:
+            # if item exists, increase the quantity by one
+            new_quantity = item[0] + 1
+            cr.execute(
+                "UPDATE orders SET quantity = ? WHERE name = ?", (new_quantity, name)
+            )
+        else:
+            # if item doesn't exist, then add the row to the db with quantity 1
+            cr.execute(
+                "INSERT INTO orders (name, price, quantity) VALUES (?, ?, 1)",
+                (name, price),
+            )
+
+        conn.commit()
+        conn.close()
+
     return redirect(url_for("main"))
 
 
 # route setting to snacks.html
 @app.route("/snacks")
 def snacks():
-    global orders
     conn = sqlite3.connect("database.db")
     cr = conn.cursor()
-    cr.execute("SELECT * FROM pizza")
+    cr.execute("SELECT * FROM snack")
     data = cr.fetchall()
     conn.close()
 
@@ -106,7 +126,9 @@ def snacks():
     # rounding to 2dp in case decimal place goes over 2
     total_cost = round(total_cost, 2)
 
-    return render_template("snacks.html", orders=orders, total_cost=total_cost)
+    return render_template(
+        "snacks.html", data=data, orders=orders, total_cost=total_cost
+    )
 
 
 # route setting to drinks.html
