7a4ad0749d105de7671844eef3c0791dc505076d
wndx2 <22222w@duck.com>
2026-05-22 12:31:49 +1200

    style: disable flask request logs
    werkzeug?
    
    Co-Authored-By: James Park <parkj2@duck.com>

--- imports.py
+++ imports.py
@@ -2,6 +2,11 @@
 # you can also add variables that is going to be WIDELY used around the project
 
 from flask import Flask, app, render_template, request, redirect, url_for
+import logging
 import sqlite3
+
+# makes flask stop printing the requiest logs and startup banners
+log = logging.getLogger("werkzeug")
+log.disabled = True
 
 app = Flask(__name__)
