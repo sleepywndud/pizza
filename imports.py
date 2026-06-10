aeb165d3909b755c659630dfdc6ba6e81d8f0342
wndx2 <22222w@duck.com>
2026-06-10 13:12:02 +1200

    chore(backup): backup 10.06.2026 state
    Co-Authored-By: James (Juyoung) Park <parkj2@duck.com>

--- imports.py
+++ imports.py
@@ -2,8 +2,8 @@
 # you can also add variables that is going to be WIDELY used around the project
 
 from flask import Flask, app, render_template, request, redirect, url_for, flash
-from colorama import Fore
-import logging
+from colorama import Fore  # colorama is for color terminal printing
+import logging  # to disable werkzeug (or whateevr) log that spams the screen
 import sqlite3
 
 # makes flask stop printing the request logs and startup banners
